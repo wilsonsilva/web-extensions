be480a7d71a0f1d047ed4cdb724baa270ceec0a0
Wilson Silva <wilson.dsigns@gmail.com>
2017-08-30 09:20:42 +0100

    Document the signal class

--- src/core/signal.js
+++ src/core/signal.js
@@ -1,7 +1,8 @@
 // @flow
 
 /**
- * Implements the observer design pattern.
+ * A Signal is essentially a mini-dispatcher specific to one event, with its own array of listeners. Listeners
+ * subscribe to real objects, not to string-based channels. Signals are inspired by C# events and signals/slots in Qt.
  */
 class Signal {
   listeners:Array<Function>
@@ -10,20 +11,40 @@ class Signal {
     this.listeners = []
   }
 
+  /**
+   * Registers an event listener object with a signal object so that the listener receives notification
+   * of an event.
+   *
+   * @param {Function} callback The function to be called when an event is dispatched.
+   */
   addListener(callback:Function) {
     this.listeners.push(callback)
   }
 
+  /**
+   * Removes a listener from the signal object. If there is no matching listener registered with the signal object,
+   * a call to this method has no effect.
+   *
+   * @param {Function} callback The function to be called when an event is dispatched.
+   */
   remove(callback:Function) {
     this.listeners = this.listeners.filter(listener =>
       listener.toString() !== callback.toString()
     )
   }
 
+  /**
+   * Similar to remove(), but removes all listeners from the signal object.
+   */
   removeAll() {
     this.listeners = []
   }
 
+  /**
+   * Dispatches an event into the event flow.
+   *
+   * @param data
+   */
   dispatch(data:any) {
     this.listeners.forEach(listener => listener(data))
   }
